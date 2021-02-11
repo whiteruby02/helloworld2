0f893b3692666eefea3d717addd7923f8bb9e24a
whiteruby02 <whiteruby02@hanmail.net>
2021-02-11 23:29:45 +0900

    Initial Commit

---
+++ helloworld2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  helloworld2
+//
+//  Created by 최준호 on 2021/02/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
